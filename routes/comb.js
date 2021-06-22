d25672d6c62ac0833095a3bdbee63ef8b3f53ccc
Dk <496908054@qq.com>
2021-06-23 00:00:47 +0800

    comb api valid compelete

--- routes/comb.js
+++ routes/comb.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Comb = require('../models/combination');
 const Part = require('../models/part');
-const events = require('events');
+const _ = require('lodash');
 // 获取组合列表
 router.get('/comb', (req, res) => {
   if (!req.query.pagenum || req.query.pagenum <= 0) {
@@ -25,33 +25,6 @@ router.get('/comb', (req, res) => {
 })
 // 添加组合信息
 router.post('/comb', (req, res) => {
-  // 臂杆校验
-  let mainboom_length = 0
-  let flyjib_length = 0
-  let unfindList = []
-  const myEventEmitter = new events.EventEmitter
-  myEventEmitter.on('valid', validLength)
-
-  req.body.comb_mainboom.forEach((item, index) => {
-    Part.findOne({ part_codename: item }, (err, result) => {
-      if (err) return console.log(err);
-      if (!result) {
-        unfindList.push(item)
-        return console.log(unfindList);
-      }
-      // console.log(result);
-      else {
-        mainboom_length += result.part_length
-        console.log(mainboom_length);
-      }
-    })
-  })
-  if (mainboom_length !== req.body.comb_mainboom_length) {
-    console.log(req.body.comb_mainboom_length);
-    console.log(mainboom_length);
-    return res.sendResult(null, 400, '杆长校验失败，请检查！')
-  }
-
   const newComb = new Comb({
     device_id: req.body.device_id,
     comb_name: req.body.comb_name,
@@ -65,12 +38,41 @@ router.post('/comb', (req, res) => {
     // 副臂组合
     comb_flyjib: req.body.comb_flyjib,
   })
-
-  newComb.save((err, result) => {
-    if (err) return res.sendResult(err, 400, '添加组合信息失败')
-    else { myEventEmitter.emit('valid') }
-    // res.sendResult(result, 201, '添加组合信息成功')
+  // 臂杆校验
+  let mainboom_length = 0  //主杆长
+  let flyjib_length = 0  //副臂长
+  let findList = []  //定义部件
+  let unfindList = [] //未定义部件
+  // console.log(req.body.comb_mainboom);
+  const promises = req.body.comb_mainboom.map(item => {
+    return Part.findOne({ part_codename: item })
   })
+  // 进行部件校验
+  Promise.all(promises).then(result => {
+    result.filter(Boolean).forEach(item => {
+      mainboom_length += item.part_type === '1' ? item.part_length : 0
+      flyjib_length += item.part_type === '2' ? item.part_length : 0
+      findList.push(item.part_codename)
+    })
+    unfindList = _.difference(req.body.comb_mainboom, findList)
+    console.log(findList, unfindList, mainboom_length, flyjib_length);
+  }).catch(err => {
+    console.log(err);
+  }).then(() => {
+    if (!(_.isEmpty(unfindList))) return res.sendResult(unfindList, 400, `${unfindList}部件未定义`)
+    if (mainboom_length != req.body.comb_mainboom_length || flyjib_length != req.body.comb_flyjib_length) {
+      return res.sendResult(null, 400, '长度错误，添加组合信息失败')
+    }
+    newComb.save((err, result) => {
+      if (err) return res.sendResult(err, 400, '添加组合信息失败')
+      const resultData = {}
+      resultData['valid_mainboom_length'] = mainboom_length
+      resultData['valid_flyjib_length'] = flyjib_length
+      resultData['comb'] = result
+      res.sendResult(resultData, 201, '添加组合信息成功')
+    })
+  })
+
 })
 // 更改组合信息
 router.put('/comb/:id', (req, res) => {
