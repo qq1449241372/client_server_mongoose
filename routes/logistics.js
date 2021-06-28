2057b07f2e2c9e039d7c7dd879d7bb98d7e88dfa
Dk <496908054@qq.com>
2021-06-29 01:05:13 +0800

    修改物流添加接口

--- routes/logistics.js
+++ routes/logistics.js
@@ -31,8 +31,12 @@ router.post('/logistics', (req, res) => {
     comb_flyjib_length: req.body.fly_jib
   }
   Comb.findOne(comb_query).then(result => {
-    plist = _.concat(result.comb_mainboom, result.comb_flyjib)
-    const promises = plist.map(item => {
+    return result
+  }).then(result => {
+    const promises = result.comb_mainboom.map(item => {
+      return Part.findOne({ part_codename: item })
+    })
+    const promises1 = result.comb_flyjib.map(item => {
       return Part.findOne({ part_codename: item })
     })
     // 0 主机 1主臂 2副臂 3桅杆 4吊钩 5配重 6拉板（连接件）7其它
@@ -40,44 +44,52 @@ router.post('/logistics', (req, res) => {
     const promises2 = queryCode.map(item => {
       return Part.find({ device_id: req.body.device_id, part_type: item })
     })
-    Promise.all(promises).then(boom_list => {
-      Promise.all(promises2).then(part_list => {
-        let sum_weight = 0
-        console.log(part_list);
-        location = boom_list[0].part_location
-        // 进行臂杆重量累加
-        boom_list.forEach(item => {
-          sum_weight += item.part_weight
+    return Promise.all(_.concat(promises, promises1, promises2)).then(part_list => {
+      // console.log(part_list);
+      return part_list
+    })
+  }).then(part_list => {
+    // console.log(part_list);
+    let sum_weight = 0
+    let main_boom = []
+    let fly_jib = []
+    let main_part = []
+    location = part_list[0].part_location
+    console.log(location);
+    part_list.forEach(item => {
+      // console.log(item);
+      main_boom.push(item.part_type === '1' ? item : null)
+      fly_jib.push(item.part_type === '2' ? item : null)
+      if (item.part_weight) {
+        sum_weight += item.part_weight
+      }
+      else {
+        item.forEach(subItem => {
+          main_part.push(subItem.part_type === '0' ? subItem : null)
+          sum_weight += subItem.part_weight
         })
-        // 进行常用部件重量累加
-        part_list.forEach(item => {
-          item.forEach(subItem => {
-            sum_weight += subItem.part_weight
-          })
-        })
-        // console.log(boom_list);
-        // console.log(part_list);
-        // console.log(location);
-        console.log(sum_weight);
-        // 查找主机部件
-        const newLogistics = new Logistics({
-          device_id: req.body.device_id,
-          main_part: part_list[0],
-          main_boom: _.dropRight(boom_list, result.comb_flyjib.length),
-          fly_jib: _.drop(boom_list, result.comb_mainboom.length),
-          // weight: req.body.weight,
-          // hook: req.body.hook,
-          // others: req.body.others,
-          from: location,
-          destination: req.body.destination,
-          sum_weight: sum_weight
-        })
-        // console.log(newLogistics);
-        newLogistics.save((err, doc) => {
-          if (err) res.sendResult(err, 400, '添加物流信息失败')
-          res.sendResult(201, doc, '添加物流信息成功')
-        })
-      })
+      }
+    })
+    console.log(main_part.filter(Boolean));
+    // console.log(main_boom.filter(Boolean));
+    // console.log(fly_jib.filter(Boolean));
+    // console.log(sum_weight);
+    const newLogistics = new Logistics({
+      device_id: req.body.device_id,
+      main_part: main_part.filter(Boolean),
+      main_boom: main_boom.filter(Boolean),
+      fly_jib: fly_jib.filter(Boolean),
+      // weight: req.body.weight,
+      // hook: req.body.hook,
+      // others: req.body.others,
+      from: location,
+      destination: req.body.destination,
+      sum_weight: sum_weight
+    })
+    console.log(newLogistics);
+    newLogistics.save((err, doc) => {
+      if (err) res.sendResult(err, 400, '添加物流信息失败')
+      res.sendResult(201, doc, '添加物流信息成功')
     })
   })
 })
